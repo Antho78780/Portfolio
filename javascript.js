72fa01c9dc199069d4e423f196b145ce592ef71e
Anthony Audrain <anthonyaud78@gmail.com>
2024-01-10 15:52:26 +0100

    add event-click

---
+++ javascript.js
@@ -0,0 +1,10 @@
+let container_header = document.getElementById("container_header");
+
+let btns = container_header.getElementsByClassName("nav-link");
+for(let i=0;i<btns.length;i++){
+    btns[i].addEventListener("click", function() {
+        let current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace("active", "");
+        this.className += " active";
+    })
+}
