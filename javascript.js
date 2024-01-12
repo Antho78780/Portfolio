d57afe9b089cd8358fe2d374a9aeef609cf176d5
Anthony Audrain <anthonyaud78@gmail.com>
2024-01-12 17:37:47 +0100

    ù

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 let container_header = document.getElementById("container_header");
-
 let btns = container_header.getElementsByClassName("nav-link");
+
 for(let i=0;i<btns.length;i++){
     btns[i].addEventListener("click", function() {
         let current = document.getElementsByClassName("active");
