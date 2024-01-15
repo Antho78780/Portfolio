c61376d8da2f043d32daf0665b816071e04d5e8c
Anthony Audrain <anthonyaud78@gmail.com>
2024-01-15 23:59:43 +0100

    test

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 let container_header = document.getElementById("container_header");
 let btns = container_header.getElementsByClassName("nav-link");
+let test = document.getElementById("test")
+
 
 for(let i=0;i<btns.length;i++){
     btns[i].addEventListener("click", function() {
@@ -8,3 +10,14 @@ for(let i=0;i<btns.length;i++){
         this.className += " active";
     })
 }
+async function displayProjects() {
+    const response = await fetch("http://127.0.0.1:5500/file.json");
+    const article = await response.json();
+    for (let i=0; i < article["projects"].length;i++) {
+        console.log(article["projects"][i])
+        if (test){
+            test.innerHTML += `<div class="col"><div class="card shadow-sm"><img src="${article["projects"][i]["image"]}" alt="" class="card-img-top bd-placeholder-img" width="100" height="225"><div class="card-body bg-light"><p class="card-text">${article["projects"][i]["text"]}</p><div class="d-flex justify-content-between align-items-center"><div class="btn-group"><a href="${article["projects"][i]["link"]}" target="_blank"><button type="button" class="btn btn-sm btn-outline-secondary">Github</button></a></div></div></div></div></div>`
+        } 
+    }
+  }
+displayProjects()
