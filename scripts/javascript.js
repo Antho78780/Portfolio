dedc294a186312acbbd17ba48b6dc74c53029ecd
Anthony Audrain <anthonyaud78@gmail.com>
2024-01-16 01:13:45 +0100

    add link dynamique

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,6 +1,7 @@
-let container_header = document.getElementById("container_header");
-let btns = container_header.getElementsByClassName("nav-link");
-let test = document.getElementById("test")
+const container_header = document.getElementById("container_header");
+const btns = container_header.getElementsByClassName("nav-link");
+let insert_project = document.getElementById("insert_project")
+let insert_language = document.getElementById("insert_language")
 
 
 for(let i=0;i<btns.length;i++){
@@ -10,14 +11,25 @@ for(let i=0;i<btns.length;i++){
         this.className += " active";
     })
 }
+
 async function displayProjects() {
     const response = await fetch("http://127.0.0.1:5500/file.json");
     const article = await response.json();
     for (let i=0; i < article["projects"].length;i++) {
-        console.log(article["projects"][i])
-        if (test){
-            test.innerHTML += `<div class="col"><div class="card shadow-sm"><img src="${article["projects"][i]["image"]}" alt="" class="card-img-top bd-placeholder-img" width="100" height="225"><div class="card-body bg-light"><p class="card-text">${article["projects"][i]["text"]}</p><div class="d-flex justify-content-between align-items-center"><div class="btn-group"><a href="${article["projects"][i]["link"]}" target="_blank"><button type="button" class="btn btn-sm btn-outline-secondary">Github</button></a></div></div></div></div></div>`
-        } 
+        insert_project.innerHTML += `<div class="col"><div class="card shadow-sm"><img src="${article["projects"][i]["image"]}" alt="" class="card-img-top bd-placeholder-img"
+        width="100" height="225"><div class="card-body bg-light"><p class="card-text">${article["projects"][i]["text"]}</p><div class="d-flex justify-content-between
+        align-items-center"><div class="btn-group"><a href="${article["projects"][i]["link"]}" target="_blank"><button type="button" class="btn btn-sm btn-outline-secondary">
+        Github</button></a></div></div></div></div></div>`
     }
-  }
+}
+async function displayLanguages() {
+    const response = await fetch("http://127.0.0.1:5500/file.json");
+    const article = await response.json();
+    for (let i=0; i < article["languages"].length;i++) {
+        insert_language.innerHTML += `<div class="col"><div class="card shadow-sm bg-light"><img src="${article["languages"][i]["image"]}"><span class="text-center">${article["languages"][i]["text"]}</span></div></div>`
+    }
+}
 displayProjects()
+displayLanguages()
+
+
