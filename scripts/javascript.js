a9d64085ac550b71861eb6e824224cacf35c1318
Anthony Audrain <anthonyaud78@gmail.com>
2024-01-19 01:30:42 +0100

    add h-100 in card and logo github

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -15,9 +15,9 @@ fetch("file.json")
     .then(res => res.json())
     .then(data => {
         for (let i=0; i < data["projects"].length;i++) {
-            insert_project.innerHTML += `<div class="col-md-4"><div class="card"><img class="card-img-top img-thumbnail" src="${data["projects"][i]["image"]}" 
-            alt="Card image cap"><div class="card-body"><p class="card-text">${data["projects"][i]["text"]}</p><a href="${data["projects"][i]["link"]}" 
-            class="btn btn-primary">Github</a></div></div></div>`
+            insert_project.innerHTML += `<div class="col-md-4 pb-4"><div class="card h-100"><img class="card-img-top img-thumbnail" src="${data["projects"][i]["image"]}" 
+            alt="Card image cap"><div class="card-body d-flex flex-column justify-content-between"><p class="card-text">${data["projects"][i]["text"]}</p>
+            <a href="${data["projects"][i]["link"]}" class="btn btn-primary"><i class="fa fa-brands fa-github me-2"></i>Github</a></div></div></div>`
         }
         for (let i=0; i < data["languages"].length;i++) {
             insert_language.innerHTML += `<div class="card" style="width: 13rem;"><img src="${data["languages"][i]["image"]}" class="card-img-top" alt="...">
